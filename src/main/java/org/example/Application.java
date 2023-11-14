0124f2905b7615315e2e706777615ee13eaf80b0
Aditya <2020wa15650@wilp.bits-pilani.ac.in>
2023-11-14 21:52:26 +0530

    updated Application.java

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -4,13 +4,47 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Application {
-//    Scanner scanner=new Scanner(System.in);
-    public static void main(String[] args) throws FileNotFoundException {
-      FileOperation fileOperation=new FileOperation();
-      fileOperation.createFile();
-      fileOperation.listFiles();
-      fileOperation.fileWrite();
-      fileOperation.readOps();
-      fileOperation.deleteFile();
+  public static FileOperation fileOperation=new FileOperation();
+  public static Scanner scanner=new Scanner(System.in);
+    public static void main(String[] args) {
+        
+        firstWindow();
+    }
+
+    public static void firstWindow(){
+        System.out.print("You can do following operation in this application");
+        System.out.println("For creating file in a directory: Enter1");
+        System.out.println("For listing all the files in a directory: Enter2");
+        System.out.println("for writing content inside the files: Enter3");
+        System.out.println("To read the content from file: Enter4");
+        System.out.println("To delete the particular file: Enter5");
+
+       int input1= scanner.nextInt();
+       scanner.nextLine();
+        System.out.println(input1);
+
+        switch (input1){
+            case 1:
+                fileOperation.createFile();
+                break;
+
+            case 2:
+                fileOperation.listFiles();
+                break;
+            case 3:
+                fileOperation.fileWrite();
+                break;
+            case 4:
+                fileOperation.readOps();
+                break;
+            case 5:
+                fileOperation.deleteFile();
+              break;
+            default:
+                System.out.println("invalid input!!");
+
+
+        }
+
     }
 }
